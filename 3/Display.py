d83fde1bb8950eb449fb028ddada939ae4845937
Koda <imaclooser@gmail.com>
2021-10-12 23:23:35 -0400

    Added Ex.3

---
+++ 3/Display.py
@@ -0,0 +1,6 @@
+# Display.py
+
+# Prints input to the Display
+class DisplayPrint:
+    def displayProduct(inp):
+        print("Display: \n" , inp)
---
+++ 3/TicketPrinter.py
@@ -0,0 +1,7 @@
+# TicketPrinter.py
+
+# Prints input as ticket
+class TicketPrint:
+
+    def displayProduct(inp):
+        print("Ticket: \n", inp)
